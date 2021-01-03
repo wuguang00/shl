3cb39f57bde280901abee6fd5cab5473914b8d4b
wuguang <wuguang20084520@163.com>
2021-01-03 22:11:55 +0800

    Updated the configuration as macbook pro.

--- maccfg/updatecfg.sh
+++ maccfg/updatecfg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 # This is case used for explanation for the menu in script
 
-cp ~/.vimrc ./
-cp ~/.bash_profile ./
-cp -rf ~/.vim ./
+\cp ~/.vimrc ./
+\cp ~/.bash_profile ./
+\cp -rf ~/.vim ./
 
