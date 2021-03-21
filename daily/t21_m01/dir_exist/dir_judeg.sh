2e93ead67f16523ab0daefb96653ded198e0693f
wuguang00 <wuguang20084520@163.com>
2021-03-21 23:33:09 +0800

    Practice the basic usage of a shell command.

---
+++ daily/t21_m01/dir_exist/dir_judeg.sh
@@ -0,0 +1,44 @@
+#!/bin/bash 
+
+if ! [ -d build ]
+then 
+    echo "build directory doesn't exists."
+    echo "Now it will be created."
+    mkdir build
+else
+    echo "build directory exists."
+fi
+
+if ! [[ -d build ]]
+then 
+    echo "build directory doesn't exists."
+    echo "Now it will be created."
+    mkdir build
+else
+    echo "build directory exists."
+fi
+
+echo "Confirmed."
+echo "Now the directory exists."
+
+
+if ! [ -d build/linux2 ]
+then 
+    echo "build/linux2 directory doesn't exists."
+    echo "Now it will be created."
+    mkdir build/linux2
+else
+    echo "build/linux2 directory exists."
+fi
+
+
+
+
+if ! [[ -d build/linux2 ]]
+then 
+    echo "build/linux2 directory doesn't exists."
+    echo "Now it will be created."
+    mkdir build/linux2
+else
+    echo "build/linux2 directory exists."
+fi
