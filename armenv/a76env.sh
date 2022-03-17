1ba393cb631904b971dbdcbb825f8ca78d0b80c1
wuguang <wuguang20084520@163.com>
2022-03-17 16:50:01 +0800

    Tried the flow for upload

--- armenv/a76env.sh
+++ armenv/a76env.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export PATH=/opt/gcc-linaro-7.4.1-2019.02-x86_64_aarch64-linux-gnu/bin:$PATH
 export PATH=/opt/gcc-arm-none-eabi-8-2019-q3-update/bin:$PATH
 export LD_LIBRARY_PATH=/opt/glibc-2.14/lib
