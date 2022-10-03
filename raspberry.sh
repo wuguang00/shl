49eaab563f2dc04d1757592d489e2129dc75bb42
wuguang00 <wuguang20084520@163.com>
2022-10-03 18:15:29 +0800

    Commit from my RPi. Try the basic flow for github again.

---
+++ raspberry.sh
@@ -0,0 +1,122 @@
+1. you should install gvim for convinience.
+
+uname -a
+whoami
+ifconfig
+git status
+dir
+uname -a
+df -h
+gvim
+vi
+which vi
+sudo shutdown -h now
+cd
+pwd
+clear
+git --version
+ifconfig
+mkdir projects
+cd projects/
+git clone git@github.com:wuguang00/shl.git
+git clone git@github.com:wuguang00/shl.git
+git clone https://github.com/wuguang00/shl.git
+ls
+cd shl/
+ls
+cd linux/
+dir
+ls
+ls -alt
+cp -rf .vim  ~/
+cp .vimrc ~/
+cp .bashrc ~/
+pwd
+x
+gc
+gvim
+vi raspberry.sh
+sudo apt update
+cd shl/
+sudo vi /etc/apt/sources.list
+{
+# deb http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi
+deb     https://mirrors.aliyun.com/raspbian/raspbian/ buster main contrib non-free rpi
+deb-src https://mirrors.aliyun.com/raspbian/raspbian/ buster main contrib non-free rpi
+# Uncomment line below then 'apt-get update' to enable 'apt-get source'
+# deb-src http://raspbian.raspberrypi.org/raspbian/ bullseye main contrib non-free rpi
+}
+# Config the disk space
+sudo raspi-config
+sudo apt-get update
+
+vim --version
+# Install gvim
+sudo apt-get remove vim-common
+sudo apt-get install vim
+sudo apt-get install vim-gui-common
+
+
+ifconfig
+git clone git@github.com:wuguang00/shl.git
+git clone git@github.com:wuguang00/shl.git
+git clone https://github.com/wuguang00/shl.git
+cp -rf .vim  ~/
+cp .vimrc ~/
+cp .bashrc ~/
+sudo apt update
+sudo vi /etc/apt/sources.list
+sudo raspi-config
+sudo apt-get update
+sudo apt-get install vim
+sudo apt-get remove vim-common
+sudo apt-get install vim
+sudo apt install ctags
+vim
+gvim
+g raspberry.sh 
+git add raspberry.sh 
+git commit -m "Added usage for RaspberryPi."
+git git config --global user.email "wuguang20084520@163.com"
+git config --global user.email "wuguang20084520@163.com"
+git config --global user.name "wuguang00"
+git status
+git commit -m "Added usage for RaspberryPi."
+git push origin HEAD:refs/for/master
+
+# 树莓派如何设置中文输入法
+sudo apt-get install fonts-wqy-microhei scim-pinyin
+# Then you can set Menu-preference.
+sudo apt-get install scim-pinyin
+# Another better one.
+sudo apt-get install fcitx fcitx-googlepinyin
+# 左上角树莓图标进入Preference -> Fcitx Configuration 或者 右击状态栏的小键盘图标进
+
+# 安装五笔输入法
+# 现在我们再来安装五笔输入法，当然它们也是建立在fcitx输入法框架上的。
+# 1.安装五笔输入法：
+sudo apt-get install -y fcitx-table-wubi
+# 如果你想安装五笔和拼音都能用的输入法——拼音五笔混合输入法，可以执行：
+sudo apt-get install -y fcitx-table-wbpy
+# 2.安装完成后你同样需要到Preference -> Fcitx Configuration选项里添加五笔输入法，就跟添加谷歌中文输入法一样。
+
+
+# Use RaspberryPi to plot Server
+NextCloud
+OpenELEC/Kodi系统
+
+
+RaspBerryPi
+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
+
+acc_all:
+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
+
+
+git remote set-url origin https://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@github.com/wuguang00/shl.git
+
+
+
+
+
+
